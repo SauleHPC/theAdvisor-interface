40b31d70df65e6fa64607803212656f3aa526d45
Erik Saule <esaule@charlotte.edu>
2025-03-23 14:49:16 +0000

    refactor

--- extractTable.py
+++ extractTable.py
@@ -1,6 +1,7 @@
 import gzip
+import sys
 
-def extract_tables(sql_file, tables):
+def extract_tables(sql_file, tables, prefix):
     encodings_to_try = ['utf-8']
     current_table = None
 
@@ -19,7 +20,9 @@ def extract_tables(sql_file, tables):
                             current_table = None
 
                     if current_table is not None:
-                        write_line(current_table, line)
+                        file_name = f'{prefix}_{current_table}.sql.gz'
+                        with gzip.open(file_name, 'at', encoding='utf-8') as outf:
+                            outf.write(line)
 
                 detected_encoding = encoding
                 break
@@ -30,12 +33,22 @@ def extract_tables(sql_file, tables):
         print("Failed to detect encoding or decode file.")
         return
 
-def write_line(table, line):
-    file_name = f'csx_db_7_15_2014_{table}.sql.gz'
-    with gzip.open(file_name, 'at', encoding='utf-8') as f:
-        f.write(line)
+def usage ():
+    print("Usage: python3 extractTable.py [sql_file] [prefix_output]")
+    sys.exit(-1)
+
 
 sql_file = '../../../mnt/large_data/csx_db_7_15_2014.sql.gz'
-tables_to_extract = ['citations']
+#tables_to_extract = ['citations']
+tables_to_extract = ['citations', 'authors', 'papers', 'paperVersions' ]
+prefix = 'csx_db_7_15_2014'
 
-extract_tables(sql_file, tables_to_extract)
+if len(sys.argv) > 1:
+    if sys.argv[1] == '--help':
+        usage()
+    sql_file = sys.argv[1]
+
+if len(sys.argv) > 2:
+    prefix = sys.argv[2]
+
+extract_tables(sql_file, tables_to_extract, prefix)
