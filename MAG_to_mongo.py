94394a5ffe2eb660e1e1c11310ee2454347369ac
Erik Saule <esaule@charlotte.edu>
2025-03-23 20:43:38 +0000

    splitting code that import raw files

--- MAG_to_mongo.py
+++ MAG_to_mongo.py
@@ -4,30 +4,6 @@ from pymongo import MongoClient
 import pandas as pd
 import gzip
 
-'''
-#DBLP
-#-----------------------------
-# Connect to the MongoDB server
-client = MongoClient('mongodb://localhost:27017/')
-# Select the database
-db = client["theAdvisor"]
-
-# Select the collection
-# Collection name
-collection_name = "Original_DBLP"
-papers_collection = db[collection_name]
-
-for i in range(0, 6800000, 50000):
-    paper_docs = []
-
-    callbacks = [lambda currentPaper: populate_dblp_mongo(currentPaper, paper_docs)]
-
-    print("parsing DBLP")
-    paper_docs = parse_DBLP_file(callbacks, i, i+50000)
-
-    papers_collection.insert_many(paper_docs)
-
-'''
 #MAG
 #-----------------------------------
 selected_columns = [0, 2, 4, 7, 9]
@@ -36,9 +12,11 @@ gz_file_path = "../../../mnt/large_data/MAG-2021-12/Papers.txt.gz"
 client = MongoClient('mongodb://localhost:27017/')
 batch_size = 50000 
 
-db = client['theAdvisor']
+mongo_database = os.getenv('MONGO_DATABASE') or "theAdvisor"
+db = client[mongo_database]
 collection_name = "Original_MAG"
 papers_collection = db[collection_name]
+
 
 with gzip.open(gz_file_path, 'rt', encoding='utf-8') as f:
     batch_counter = 0
