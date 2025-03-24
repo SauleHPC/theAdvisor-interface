e21ffb9a156282ef6c041aaa50eed67a262d79e1
Erik Saule <esaule@charlotte.edu>
2025-03-24 02:56:25 +0000

    MAG import freshened up

--- MAG_to_mongo.py
+++ MAG_to_mongo.py
@@ -7,7 +7,8 @@ import os
 
 #MAG
 #-----------------------------------
-selected_columns = [0, 2, 4, 7, 9]
+selected_columns = [0, 2, 4, 7]
+column_names= ["MAGid", "DOI", "title", "year"]
 gz_file_path = "../../../mnt/large_data/MAG-2021-12/Papers.txt.gz"
 
 client = MongoClient('mongodb://localhost:27017/')
@@ -19,11 +20,19 @@ collection_name = "Original_MAG"
 papers_collection = db[collection_name]
 
 
+flush_collection = True
+if flush_collection:
+    print ("Deleting {collection_name}")
+    result = papers_collection.delete_many({})
+    print(f"Deleted {result.deleted_count} documents from {collection_name}.")
+
+print ("parsing")
+    
 with gzip.open(gz_file_path, 'rt', encoding='utf-8') as f:
     batch_counter = 0
     
     for chunk in pd.read_csv(f, sep='\t', header=None, usecols=selected_columns, chunksize=batch_size):
-        chunk.columns = [f"col_{i}" for i in range(len(selected_columns))]
+        chunk.columns = [column_names[i] for i in range(len(selected_columns))]
         
         data = chunk.to_dict(orient='records')
         
@@ -31,4 +40,4 @@ with gzip.open(gz_file_path, 'rt', encoding='utf-8') as f:
         
         batch_counter += len(data)
 
-        print (batch_size)
+        print (batch_counter)
