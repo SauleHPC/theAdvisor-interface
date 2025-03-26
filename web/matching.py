777811644555f2a8ea73dea73881e184afe9b131
Erik Saule <esaule@charlotte.edu>
2025-03-26 02:36:56 +0000

    towards database

--- web/matching.py
+++ web/matching.py
@@ -2,6 +2,7 @@ import networkx as nx
 from pymongo import MongoClient
 import os
 import sys
+from datetime import datetime
 
 '''
 File is used as a simple matching script to cross refrence paper in MongoDB.
@@ -16,7 +17,8 @@ db = client[db_name]
 DBLPtoMAG = db['DBLPtoMAG']
 DBLPtoCiteseer = db['DBLPtoCiteseer'] 
 Original_DBLP = db['Original_DBLP'] 
-Original_MAG = db['Original_MAG'] 
+Original_MAG = db['Original_MAG']
+papers_Citeseer = db['papers_Citeseer'] 
 MAG_Ref = db['Original_MAG_References']
 
 #DBLP to Citeseer
@@ -48,10 +50,10 @@ def make_matching_graph():
     #first add all DBLP papers
     original_dblp_papers = Original_DBLP.find({}, {'paper_id': 1, '_id': 0})
 
-    for paper in original_dblp_papers:
-        paper_id = paper['paper_id']
-
-        G.add_node(dblp_node_name(paper_id), data = dblp_node_data(paper_id))
+    if True: #delete me
+        for paper in original_dblp_papers:
+            paper_id = paper['paper_id']
+            G.add_node(dblp_node_name(paper_id), data = dblp_node_data(paper_id))
 
     print ("parsing DBLP to MAG data")
     # go through all DBLP to MAG match
@@ -64,6 +66,8 @@ def make_matching_graph():
         if dblp_node_name(dblp_id) not in G.nodes: #shouldn't happen unless files are out of sync
             G.add_node(dblp_node_name(dblp_id), data = dblp_node_data(dblp_id))
         G.add_edge(dblp_node_name(dblp_id), mag_node_name(mag_id))
+        if len(G.nodes) > 100000000: #delete me
+            break
 
     print ("parsing DBLP to Citeseer data")
     # go through all DBLP to Citeseer match
@@ -75,47 +79,126 @@ def make_matching_graph():
         if citeseer_node_name(citeseer_id) not in G.nodes:
             G.add_node(citeseer_node_name(citeseer_id), data = citeseer_node_data(citeseer_id))
         if dblp_node_name(dblp_id) not in G.nodes:
-            G.add_node(citeseer_node_name(dblp_id), data = dblp_node_data(dblp_id))
+            G.add_node(dblp_node_name(dblp_id), data = dblp_node_data(dblp_id))
         G.add_edge(dblp_node_name(dblp_id), citeseer_node_name(citeseer_id))
+        if len(G.nodes) > 200000000: #delete me
+            break
 
+        
     
     return G
 
+
+def add_mag_dist_1(graph):
+    mag_seen=set()
+
+    print(datetime.now())
+    print ("Logging seen MAG papers")
+    for v in graph.nodes:
+        try :
+            if graph.nodes[v]['data']['src'] == 'MAG':
+                mag_id = graph.nodes[v]['data']['id']
+                mag_seen = mag_seen.union( set([mag_id]) )
+        except Exception as e:
+            print ("Exception: "+str(e))
+            print (f"vertex at exception time {v}")
+
+    print (f"{len(mag_seen)} mag papers in")
+        
+    mag_dist_0 = mag_seen.copy()
+
+    print(datetime.now())
+    print ("Adding dist 1 papers from MAG")
+    #add all mag papers within 1 of the one we matched to get a real graph
+    magrefcount = 0
+    newmagpapers = 0
+    for mag_edge in MAG_Ref.find({},{"citer":1, "citee":1, "_id":0}):
+        if magrefcount % 1000000 == 0:
+            print (f"{magrefcount} MAG edges")
+            print (f"{newmagpapers} new papers ")
+            print (f"magseen: {len(mag_seen)}")
+            print (f"magdist0: {len(mag_dist_0)}")
+
+            if newmagpapers > 100: #delete me
+                break
+        magrefcount = magrefcount+1
+        src_magid = mag_edge['citer']
+        dest_magid = mag_edge['citee']
+       
+        if dest_magid in mag_dist_0:
+            if src_magid not in mag_seen:
+                mag_seen = mag_seen.union( set([src_magid]) )
+                graph.add_node(mag_node_name(src_magid), data = mag_node_data(src_magid))
+                newmagpapers += 1
+            
+        if src_magid in mag_dist_0:
+            if dest_magid not in mag_seen:
+                mag_seen = mag_seen.union ( set([dest_magid]) )
+                graph.add_node(mag_node_name(dest_magid), data = mag_node_data(dest_magid))
+                newmagpapers += 1
+
+
+    print (f"dist 1 mag papers: {newmagpapers}")
+    return graph
+
+    
+print(datetime.now())
+
 graph = make_matching_graph()
 
-mag_seen=set()
+print (f"vertices: {len(graph.nodes)}")
 
-print ("Logging seen MAG papers")
-for v in graph.nodes:
-    try :
-        if graph.nodes[v]['data']['src'] == 'MAG':
-            mag_id = graph.nodes[v]['data']['id']
-            mag_seen = mag_seen & set([mag_id])
-    except Exception as e:
-        print ("Exception: "+str(e))
-        print (f"vertex at exception time {v}")
-
-print ("Adding dist 1 papers from MAG")
-#add all mag papers within 1 of the one we matched to get a real graph
-magrefcount = 0
-for mag_edge in MAG_Ref.find({},{"citer":1, "citee":1, "_id":0}):
-    if magrefcount % 1000000 == 0:
-        print (f"{magrefcount} MAG edges")
-    magrefcount = magrefcount+1
-    src_magid = mag_edge['citer']
-    dest_magid = mag_edge['citee']
-    if dest_magid in mag_seen:
-        if src_magid not in mag_seen:
-            mag_seen = mag_seen & set([src_magid])
-            graph.add_node(mag_node_name(src_magid), data = mag_node_data(src_magid))
-
-    if src_magid in mag_seen:
-        if dest_magid not in mag_seen:
-            mag_seen = mag_seen & set([dest_magid])
-            graph.add_node(mag_node_name(dest_magid), data = mag_node_data(dest_magid))
-        
+graph = add_mag_dist_1(graph)
+print(datetime.now())
 
 conn = nx.connected_components(graph)
 
+
+def make_the_advisor_paper(connected_component):
+    ret = {}
+    ret['sources'] = connected_component.copy()
+    found_dblp = None
+    found_mag = None
+    found_citeseer = None
+    for p in connected_component:
+        if graph.nodes[p]['data']['src'] == 'MAG':
+            found_mag = graph.nodes[p]['data']['id']
+        if graph.nodes[p]['data']['src'] == 'DBLP':
+            found_dblp = graph.nodes[p]['data']['id']
+        if graph.nodes[p]['data']['src'] == 'Citeseer':
+            found_citeseer = graph.nodes[p]['data']['id']
+
+    if found_dblp:
+        dblppaper = db.Original_DBLP.find_one({'paper_id': found_dblp})
+        ret ['title'] = dblppaper['title']
+    elif found_mag:
+        magpaper = db.Original_MAG.find_one({'MAGid': found_mag})
+        ret ['title'] = magpaper['title']
+    elif found_citeseer:
+        citeseerpaper = db.papers_Citeseer.find_one({'id': found_citeseer})
+        ret ['title'] = magpaper['title'] or ""
+    return ret
+
+
+print ("flushing")
+print(datetime.now())
+    
+theadvisor_collection = db['theAdvisor_papers']
+
+theadvisor_collection.delete_many({}) #always flush
+
+print ("flushed")
+print(datetime.now())
+
+
+theadvisorid = 0
+
 for co in conn:
     print (co)
+    pap = make_the_advisor_paper(co)
+    pap['theadvisor_id'] = theadvisorid
+    theadvisorid +=1
+    print (pap)
+
+
+    
