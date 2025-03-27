57b485b2366963c1fa0aa671fef92a4722526bdb
Erik Saule <esaule@charlotte.edu>
2025-03-27 23:47:44 +0000

    basic page ranking

--- back_end/citation.py
+++ back_end/citation.py
@@ -18,17 +18,46 @@ def safe_add_edge(src, dest):
     graph.add_edge(src, dest)
 
 @citation_bp.route('/load')
-def load_citation_graph():
+def load_citation_graph(limit = 99999999):
     global graph
     graph = nx.DiGraph()
     for theadvisor_paper in theAdvisor_collection.find({}, {'theadvisor_id':1, 'citer':1, 'citee':1}):
         for pred in theadvisor_paper['citer']:
             safe_add_edge(pred, theadvisor_paper['theadvisor_id'])
+        if len(graph.nodes) > limit:
+            break
     return f"{len(graph.nodes)} {len(graph.edges)}"
 
+@citation_bp.route('/quickload')
+def QL():
+    load_citation_graph(10000)
+    return ""
 
-@citation_bp.route('/api/v1/citation/most_cited'):
+
+@citation_bp.route('/api/v1/citation/most_cited')
 def most_cited():
     global graph
-    
+    deg = graph.in_degree()
+    verts = [v for v in graph.nodes]
+    verts = sorted(verts, key=lambda x: deg[x])
+    return f"{verts[0]} {deg[verts[0]]}  {verts[-1]} {deg[verts[-1]]}"
 
+
+@citation_bp.route('/api/v1/citation/most_citing')
+def most_citing():
+    global graph
+    deg = graph.out_degree()
+    verts = [v for v in graph.nodes]
+    verts = sorted(verts, key=lambda x: deg[x])
+    return f"{verts[0]} {deg[verts[0]]}  {verts[-1]} {deg[verts[-1]]}"
+
+@citation_bp.route('/api/v1/citation/page_rank')
+def page_rank():
+    global graph
+    ranks = nx.pagerank(graph)
+    verts = [v for v in graph.nodes]
+    verts = sorted(verts, key=lambda x: ranks[x])
+    ret = []
+    for i in range(0, 100):
+        ret.append((verts[-i-1], ranks[verts[-i-1]]))
+    return ret
